868875ebb002b59ef45bb5f830c79f068d0f7e3b
BagusTri <11211021@student.itk.ac.id>
2023-11-06 11:35:18 +0800

    update pada tampilan aplikasi

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:provider/provider.dart';
 
 import 'firebase_options.dart';
 
